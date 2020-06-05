3e2e0d10e26ced91c338b33f7fe6d9b3a8fea4a4
61637557 <lili@Lilis-MacBook-Air.local>
2020-06-05 16:38:21 +0100

    titles creation

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,32 @@
 module.exports = (app) => {
     
         app.get("/", (req, res) => {
-        res.render("index");
+            res.render("index", {
+                title: "Cube workshop"
+        });
         })
 
         app.get("/about", (req, res) => {
-        res.render("about");
+            res.render("about", {
+                title: "About"
+        });
         })
     
         app.get("/create", (req, res) => {
-        res.render("create");
+            res.render("create", {
+            title: "Create"
+        });
         })
     
         app.get("/details/:id", (req, res) => {
-        res.render("details");
+            res.render("details", {
+            title: "Details"
+        });
         })
     
         app.get("*", (req, res) => {
-        res.render("404");
+            res.render("404", {
+            title: "Page not found"
+        });
     })
 };
