8602f59ed8cfa712efe7347d8c1aaf56c67ea4bd
61637557 <lili@Lilis-MacBook-Air.local>
2020-06-16 11:59:42 +0100

    connect to the DB

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const env = process.env.NODE_ENV || 'development';
+const mongoose = require("mongoose")
 
 const config = require('./config/config')[env];
 const express = require('express')
 const app = express();
+
+mongoose.connect(config.databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}, (err) => {
+    if (err) {
+        console.err(err);
+        throw err;
+    }
+
+    console.log("DB is setup and running")
+})
+
 
 require('./config/express')(app);
 require('./routes/index.js')(app);
